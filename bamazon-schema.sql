0585635832a93964a061350d3f232c2e4f30f79d
KevinL <mastahkevin@gmail.com>
2019-05-12 14:13:07 -0700

    Installed cli-table3 to tidy up console table. Also updated Schema.

--- bamazon-schema.sql
+++ bamazon-schema.sql
@@ -10,16 +10,16 @@ CREATE TABLE products (
     stock_quantity INT NOT NULL
 );
 
-INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES 
-('Beats Solo3 Wireless On-Ear Headphones','Electronics',224.95,15),
+INSERT INTO products (product_name,department_name,price,stock_quantity) VALUES 
+('Beats Headphones','Electronics',224.95,15),
 ('Testimony','Books',10.91,40),
-('Fellowes Powershred 73Ci','Electronics', 149.99,55),
-('Big Green Egg','Garden & Outdoor', 225, 25),
-('Nest Thermostat','Home Improvement', 220, 75),
-('TaylorMade RBZ Driver','Sports & Fitness', 199.99, 80),
-('Gumby Dog Toy', 'Pet Supplies', 3.99, 25),
+('Cracking The Coding Interview','Books',29.47,55),
+('Big Green Egg','Garden & Outdoor',225,25),
+('Nest Thermostat','Home Improvement',220,75),
+('TaylorMade Driver','Sports & Fitness',199.99,80),
+('Gumby Dog Toy','Pet Supplies',3.99,25),
 ('Magic Mouse','Electronics',99.99,33),
-('Disc Brake Rotors','Automotive',61,33),
+('Echo','Surveillance',64.99,27),
 ('Alpine Car Stereo','Electronics',110.95,35);
 
 SELECT * FROM products;
