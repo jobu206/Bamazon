ffe998dc777e0ae3a9e4bc9f56091888d2526d5a
KevinL <mastahkevin@gmail.com>
2019-05-12 14:11:22 -0700

    Installed cli-table3 and confirmed I can connect via .env file.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,17 +1,17 @@
+// dotenv file to contain db configuration
 require('dotenv').config();
-
+// required dependencies
 const mysql = require('mysql');
 const inquirer = require('inquirer');
 const table = require('cli-table3');
 const chalk = require('chalk');
 
-
+// connection script
 const connection = mysql.createConnection({
     // Host Name
     host: process.env.DB_HOST,
     // username
     user: process.env.DB_USER,
-
     // Password
     password: process.env.DB_PASS,
     database: process.env.DB_NAME
@@ -19,6 +19,8 @@ const connection = mysql.createConnection({
 
 connection.connect(function (err) {
     if (err) throw err;
-    runSearch();
+    // runSearch();
+    console.log('I\'m connected!');
+    
 });
 
