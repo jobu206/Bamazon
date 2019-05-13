3a1438b81038f81d22bdb58fb88f17dbca146eef
KevinL <mastahkevin@gmail.com>
2019-05-13 08:13:12 -0700

    Conitnued refinement of both connectin function and buildout of inquirer function.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -35,10 +35,30 @@ function bamazon() {
                 head: ["Product ID".red.bold, "Product Name".red.bold, "Department Name".cyan.bold, "Price".red.bold, "Quantity".red.bold],
                 colWidths: [12, 75, 20, 12, 12],
             });
+            // 
             for (let i = 0; i < res.length; i++) {
                 table.push(
-                    [res[i].id,res[i].product]
+                    [res[i].item_id,res[i].product_name,res[i].department_name,parseFloat(res[i].price).toFixed(2),res[i].stock_quantity]
                 )
             }
+            console.log(table.toString);
+
+            // prompt shopper for input
+            inquirer.prompt([
+                {
+                    type: "number",
+                    message: "Please enter the Product ID of the item that you would like to buy.".green,
+                    name: "id"
+                },
+                {
+                    type: "number",
+                    message: "How many would you like to buy?",
+                    name: "quantity"
+                },
+            ])
+            // Ordering function
+            .then(function (cart) {
+                
+            })
     });
 }
