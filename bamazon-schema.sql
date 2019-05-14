80195c396838e6e3d75d1ba002d57718fc052252
KevinL <mastahkevin@gmail.com>
2019-05-13 19:40:33 -0700

    Updated quantities.

--- bamazon-schema.sql
+++ bamazon-schema.sql
@@ -12,14 +12,14 @@ CREATE TABLE products (
 
 INSERT INTO products (product_name,department_name,price,stock_quantity) VALUES 
 ('Beats Headphones','Electronics',224.95,15),
-('Testimony','Books',10.91,40),
-('Cracking The Coding Interview','Books',29.47,55),
-('Big Green Egg','Garden & Outdoor',225,25),
+('Testimony','Books',10.91,5),
+('Cracking The Coding Interview','Books',29.47,10),
+('Big Green Egg','Garden & Outdoor',225,3),
 ('Nest Thermostat','Home Improvement',220,75),
-('TaylorMade Driver','Sports & Fitness',199.99,80),
-('Gumby Dog Toy','Pet Supplies',3.99,25),
+('TaylorMade Driver','Sports & Fitness',199.99,15),
+('Gumby Dog Toy','Pet Supplies',3.99,20),
 ('Magic Mouse','Electronics',99.99,33),
 ('Echo','Surveillance',64.99,27),
-('Alpine Car Stereo','Electronics',110.95,35);
+('Alpine Car Stereo','Electronics',110.95,1);
 
 SELECT * FROM products;
