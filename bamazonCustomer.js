f5b11ddd06b42e63f2c76e315e9fc7de8e6dcf90
KevinL <mastahkevin@gmail.com>
2019-05-15 16:35:35 -0700

    Updating bamazonCustomer.js to buildout prompts.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -19,71 +19,28 @@ const connection = mysql.createConnection({
 });
 
 connection.connect(function (err) {
-    if (err) throw colors.red.err;
-    console.log(colors.green('Connected as id ' + connection.threadId));
-    // function to display database.
-    displayInventory();
+    if (err) throw err;
+    console.log(colors.yellow("Welcome to Bamazon! You are now connected to the Store as id " + connection.threadId));
+
+    bamazon();
 });
-// function to display inventory
-function displayInventory() {
-    connection.query('SELECT `item_id`,`prod_name`,`dept_name`,`price` FROM products', function (err, res) {
+
+function bamazon() {
+    connection.query ("SELECT * FROM products", function(err, res) {
         if (err) throw err;
-        let table = new Table (
+        // Cli-Table display code with Color
+        let table = new Table(
             {
-                head: ['Product ID'.cyan.bold, 'Product Name'.cyan.bold, 'Department Name'.cyan.bold, 'Price'.cyan.bold], colWidths: [12,75,20,12]
-            }
-        );
-        for (let i = 0; i < res.length; i++) {
+                head: ["Product ID".cyan.bold, "Product Name".cyan.bold, "Department Name".cyan.bold, "Price".cyan.bold, "Quantity".cyan.bold],
+                colWidths: [12, 75, 20, 12, 12],
+            });
+
+        // Set/Style table headings and Loop through entire inventory
+        for (var i = 0; i < res.length; i++) {
             table.push(
-                [res[i].item_id, res[i].prod_name, res[i].dept_name, parseFloat(res[i].price).toFixed(2)]
-            )
+                [res[i].item_id, res[i].prod_name, res[i].dept_name, parseFloat(res[i].price).toFixed(2), res[i].stock_qty]
+            );
         }
-        // log table in console.
         console.log(table.toString());
-        // Prompt for user to enter choice
     });
 }
-userPrompt();
-
-function userPrompt() {
-    inquirer.prompt([
-        {
-            name: 'id',
-            type: 'number',
-            message: 'Please enter the product ID of the tiem you would like to buy'.yellow
-        },
-        {
-            name: 'units',
-            type: 'number',
-            message: 'How many units would you like to buy?'
-        }
-    ])
-    .then (function (answer) {
-        console.log('answer',answer);
-        checkInventory(answer);
-    });
-}
-// function to check inventory after user chooses product to buy.
-function checkInventory(answer) {
-    connection.query('SELECT `stock_qty, `price` FROM products WHERE ?', 
-    {
-        item_id: answer.item
-    },
-    // function to show results
-    function (err, res) {
-        if (err) throw err;
-        console.log(res);
-        if (res[0].stock_qty < answer.units) {
-            console.log(colors.red('We\'re sorry, but we are unable to fulfill this request. Please check back later.'));
-            connection.end();
-        } else {
-            updateInventory(answer, res);
-            connection.end();
-        }
-    });
-}
-
-// function updateInventory(answer, res) {
-//     console.log(colors.green(res[0].));
-//     connection.query('UPDATE produc')
-// }
