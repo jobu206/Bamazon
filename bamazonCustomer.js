3c9d8f076fbcee62febfb61ce2279774982854ff
KevinL <mastahkevin@gmail.com>
2019-05-15 21:12:49 -0700

    Final product

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -8,31 +8,26 @@ const colors = require('colors');
 
 // connection script
 const connection = mysql.createConnection({
-    // Host Name
     host: process.env.DB_HOST,
-    // Username
     user: process.env.DB_USER,
-    // Password
     password: process.env.DB_PASS,
-    // DB Name
     database: process.env.DB_NAME
 });
 
 connection.connect(function (err) {
     if (err) throw err;
     console.log(colors.yellow("Welcome to Bamazon! You are now connected to the Store as id " + connection.threadId));
-
-    bamazon();
 });
 
-function bamazon() {
-    connection.query ("SELECT * FROM products", function(err, res) {
+function displayProducts() {
+    console.log(colors.green("Showing all products available."));
+    connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
         // Cli-Table display code with Color
         let table = new Table(
             {
-                head: ["Product ID".cyan.bold, "Product Name".cyan.bold, "Department Name".cyan.bold, "Price".cyan.bold, "Quantity".cyan.bold],
-                colWidths: [12, 75, 20, 12, 12],
+                head: ["ID".cyan.bold, "Product Name".cyan.bold, "Department Name".cyan.bold, "Price".cyan.bold, "Qty".cyan.bold],
+                colWidths: [5, 45, 20, 12, 12],
             });
 
         // Set/Style table headings and Loop through entire inventory
@@ -42,5 +37,61 @@ function bamazon() {
             );
         }
         console.log(table.toString());
+        placeOrder();
     });
 }
+
+function placeOrder() {
+    inquirer.prompt([
+        {
+            name: "prodId",
+            type: "number",
+            message:"Please enter Item ID you like to purchase."
+        },
+        {
+            name:"qty",
+            type:"number",
+            message:"How many items do you wish to purchase?"
+        },
+    ]).then(function(answer) {
+        checkInvetory(answer);
+    });
+};
+
+function checkInvetory(answer) {
+    connection.query("SELECT * FROM products WHERE ?",
+        {
+            item_id: answer.prodId
+        },
+        function(err, res) {
+            if (err) throw err;
+            if (res[0].stock_qty < answer.qty) {
+                console.log(colors.red("We're sorry, but we are unable to fulfill your order."));
+                connection.end();
+            } else {
+                updateDB(answer, res);
+                // connection.end();
+            }
+        }
+    );
+}
+
+function updateDB(answer, res) {
+    connection.query("UPDATE products SET ? WHERE ?", [
+        {
+            stock_qty: res[0].stock_qty - answer.qty
+        },
+        {
+            item_id: answer.prodId
+        }
+    ], 
+    function (err) {
+        if (err) throw err;
+        let num = res[0].price * answer.qty;
+        let n = num.toFixed(2);
+        console.log(colors.yellow("Your total price is: " + n));
+    })
+    displayProducts();
+}
+
+displayProducts();
